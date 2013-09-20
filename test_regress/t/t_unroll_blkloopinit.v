156806669e757cedda90ec85e409297c4f027e2d
Jeremy Bennett <jeremy.bennett@embecosm.com>
2013-09-20 08:09:28 +0100

    Test case for loop unrolling.
    test_regress:
    	* t/t_unroll_blkloopinit.pl: Created.
    	* t/t_unroll_blkloopinit.v: Created.

---
+++ test_regress/t/t_unroll_blkloopinit.v
@@ -0,0 +1,40 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This files is used to generated the BLKLOOPINIT error which
+// is actually caused by not being able to unroll the for loop.
+//
+// The reason for not being able to unroll is that the CONDITION
+// in the for loop is not a "> >= < <=".
+//
+// This file ONLY is placed into the Public Domain, for any use,
+// without warranty, 2013 by Jie Xu.
+
+module t (/*AUTOARG*/
+   // Inputs
+   clk
+   );
+   input clk;
+   reg    [3:0] tmp [3:0];
+
+   initial begin
+       tmp[2] = 4'b0010;
+       tmp[3] = 4'b0011;
+   end
+   
+
+   // Test loop
+   always @ (posedge clk) begin
+       int i;
+       // the following for-loop can't be UNROLLED
+       for(i = 0;(i < 4) && (i > 1); i++)
+       begin
+           tmp[i] <= tmp[i-i];
+       end
+
+	 if (tmp[3] != 4'b0011) $stop;
+	 $write("*-* All Finished *-*\n");
+	 $finish;
+   end
+
+endmodule
+
