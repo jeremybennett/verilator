9914551c60725a8af8765476a251ae478ebbfd3a
Jeremy in Trondheim <jeremy.bennett@atmel.com>
2013-11-15 10:00:27 +0100

    Better test of modport export.
    test_regress:
    	* t/t_interface_modport_export.v: Updated to version which will pass
    	with VCS.

--- test_regress/t/t_interface_modport_export.v
+++ test_regress/t/t_interface_modport_export.v
@@ -7,12 +7,21 @@
 
 interface test_if;
 
+   // Pre-declare function
+   extern function myfunc (input logic val);
+
    // Interface variable
    logic 	data;
 
    // Modport
-   modport mp(
+   modport mp_e(
               export  myfunc,
+	      output  data
+	      );
+
+   // Modport
+   modport mp_i(
+              import  myfunc,
 	      output  data
 	      );
 
@@ -27,27 +36,34 @@ module t (/*AUTOARG*/
 
    test_if i ();
 
-   function automatic logic i.myfunc (input logic val);
+   testmod_callee testmod_callee_i (.ie (i.mp_e));
+   testmod_caller testmod_caller_i (.clk (clk),
+				    .ii (i.mp_i));
+endmodule
+
+
+module testmod_callee
+  (
+   test_if.mp_e  ie
+   );
+
+   function automatic logic ie.myfunc (input logic val);
       begin
 	 myfunc = (val == 1'b0);
       end
    endfunction
-
-   testmod testmod_i (.clk (clk),
-		      .i (i.mp));
-
-endmodule
+endmodule // testmod_caller
 
 
-module testmod
+module testmod_caller
   (
    input clk,
-   test_if.mp  i
+   test_if.mp_i  ii
    );
 
    always @(posedge clk) begin
-      i.data = 1'b0;
-      if (i.myfunc (1'b0)) begin
+      ii.data = 1'b0;
+      if (ii.myfunc (1'b0)) begin
 	 $write("*-* All Finished *-*\n");
 	 $finish;
       end
